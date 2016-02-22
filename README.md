34fd2f8e57a4d9ddf9a0c10d5d71a531c4df940e
Erik Reppel <erikreppel@gmail.com>
2016-02-21 19:03:43 -0800

    added authentication functionality through auth0

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Chirper Workshop
 ===
 
 The repo for a workshop on building a twitter clone with MongoDB, Express, and ejs for templating.
+
+Things used:
+- [auth0](https://auth0.com/)
+- [Express](http://expressjs.com/)
+- [Express sessions](https://github.com/expressjs/session)
--- app.js
+++ app.js
@@ -1,17 +1,48 @@
 'use strict';
 
-const express = require('express'); // imports the Express library
-const app = express();              // creates a new Express app
+/*Imports**********************************************************************/
+const express = require('express');     // imports the Express library
+const passport = require('passport');   // handles auth0
+const strategy = require('./auth/setup-passport');    // import the stub we made
+const cookieParser = require('cookie-parser');        // a tool to parse cookies
+const session = require('express-session');           // used to track user sessions
+const path = require('path');
+var requiresLogin = require('./auth/requiresLogin')
+
+
+/*Globals**********************************************************************/
+var app = express();                // creates a new Express app
 const PORT = 3000;                  // keep the port we're opening as a global
 
+
+/*Middlewares******************************************************************/
+app.use(session({                       // Used to track user session
+        secret: "ASECRETCODEGOESHERE",
+        resave: false,
+        saveUninitialized: false
+    }));
+app.use(passport.initialize());         // Used for auth
+app.use(passport.session());            // Used for auth
+
+
+/*Routes***********************************************************************/
 // The route handler for index
 app.get('/', function(req, res) {
     // when a GET request is sent to '/' on our app, we apply a callback function
     // the function has request and response (req, res) params
     // req is what the server received, res is what wer are going to send back
 
-    res.send('hello world');
-})
+    res.sendFile(path.join(__dirname, 'static/index.html'));
+});
+
+// route handler for our auth callback
+app.get('/callback',
+    passport.authenticate('auth0', {failureRedirect: '/fail'}),
+    function(req, res) {
+        if (!req.user) throw new Error('user null');
+        else res.redirect('/');
+    });
+
 
 app.listen(PORT, function() {
     console.log('Listening on port', PORT);
